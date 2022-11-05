0191878bfef5ec1df027dcd50d7ea7df8021c5a0
itzbidyut <bbboruah1994@gmail.com>
2022-11-05 20:45:40 +0530

    modal added

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import PokemonModal from "./PokemonModal";
+
 export default function CardItem({ data, index }) {
+  const [modal, setModal] = useState(false);
+
+  const showModal = () => {
+    console.log("showModal==============");
+    setModal(true);
+    document.body.classList.toggle("stop-scrolling");
+  };
+  const closeModal = () => {
+    console.log("closeModal==============");
+    setModal(false);
+    document.body.classList.toggle("stop-scrolling");
+  };
   return (
     <div className="col-12 col-md-6 col-lg-4">
-      <div className="pokemon">
+      <div className="pokemon" onClick={showModal}>
         <div className={`pokemonCard ${data.types[0].type.name}`}>
           <div className="">
             <div className="index">
@@ -13,19 +28,12 @@ export default function CardItem({ data, index }) {
                 <p key={index}>{item.type.name}</p>
               ))}
             </div>
-
-            <div className="pokemonStats">
-              {data.stats.map((item) => (
-                <p>
-                  {item.stat.name} - {item.base_stat}
-                </p>
-              ))}
-            </div>
           </div>
 
           <img src={data.sprites.front_shiny} alt={data.name} />
         </div>
       </div>
+      {modal ? <PokemonModal closeModal={closeModal} data={data} /> : <></>}
     </div>
   );
 }
---
+++ src/components/PokemonModal.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+
+export default function PokemonModal({ data, closeModal }) {
+  const [stats, setStats] = useState([]);
+
+  console.log("PokemonModal=====", data);
+  console.log("data.stats.base_stat=====", data.stats[0].base_stat);
+
+  //   useEffect(() => {
+  //     setStats((list) => data.stats.base_stat);
+  //     console.log("stats===============", stats);
+  //   }, [data]);
+
+  //   const totalStat = stats.reduce(
+  //     (previousValue, currentValue) => previousValue + currentValue
+  //   );
+  //   console.log("totalStat=====", totalStat);
+  return (
+    <div className="PokemonModal">
+      <div className="pokemonBox">
+        <div onClick={closeModal} className="closeBtn">
+          <span class="material-symbols-outlined">close</span>
+        </div>
+        <div className={`row ${data.types[0].type.name}`}>
+          <div className="col-12 col-md-6">
+            <div className="pokemonLeft text-center">
+              <p className="name ">{data.name}</p>
+              <img
+                src={data.sprites.front_shiny}
+                alt={data.name}
+                className="img"
+              />
+              <div className="abilitys">
+                <p className="title">ability</p>
+                {data.abilities.map((item, index) => (
+                  <p className="ability" key={index}>
+                    {item.ability.name}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+            <div className="pokemonRight">
+              <p className="baseTitle text-center">Base Stats</p>
+              {data.stats.map((item, index) => (
+                <div key={index} className="statDetails">
+                  <p className="statName">
+                    {item.stat.name} {item.base_stat}
+                  </p>
+                  <div className="bar">
+                    <div
+                      className="percentageBar"
+                      style={{ width: `${item.base_stat}px` }}
+                    ></div>
+                  </div>
+                </div>
+              ))}
+              {/* <p>{totalStat}</p> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+//     style={{ width: `${item.base_stat}` }}
