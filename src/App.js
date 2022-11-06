4edc0e47f177f6a0d4fa8ed3d5fc1a7fd7e55af4
itzbidyut <bbboruah1994@gmail.com>
2022-11-06 09:47:29 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ function App() {
             </>
           )}
 
-          {searchValue ? (
+          {search ? (
             <></>
           ) : (
             <>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,24 @@ export default function Header({ handleSearch, setSearchValue, searchValue }) {
   return (
     <div className="headerComponent">
       <div className="container">
-        <div className="headerTitle">
-          <p className="title">Pokedex</p>
-        </div>
-        <div className="searchBox">
-          <form onSubmit={handleSearch}>
-            <input
-              value={searchValue}
-              placeholder="search pokemon"
-              onChange={(e) => setSearchValue(e.target.value)}
-            />
-            <button className="">Search</button>
-          </form>
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <div className="headerTitle">
+              <p className="title">Pokedex</p>
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+            <div className="searchBox">
+              <form onSubmit={handleSearch}>
+                <input
+                  value={searchValue}
+                  placeholder="search pokemon"
+                  onChange={(e) => setSearchValue(e.target.value)}
+                />
+                <button className="">Search</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
